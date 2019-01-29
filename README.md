80a3142fe30c0bd65b8845812821540d17b244ca
tony <tony@hampguest68-248.hampshire.edu>
2019-01-29 14:08:12 -0500

    Took out my password from source file, finish the templete for README.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# timecard_autofill
+# Autofiller
+
+Autofiller is an autofill program which helps you fill payroll time 
+card bi-weekly. It's still the alpha version but working.  
+
+
+
+### User Stories
+
+
+- [x] User can enter a day of the week and one time slot of that day so that the program will fill that one slot for the user.
+- [ ] User can enter multiple days and slots at the same time.
+- [ ] User can switch between different browsers.
+
+### App Walkthough GIF
+`TODO://` Add the URL to your animated app walkthough `gif` in the image tag below, `YOUR_GIF_URL_HERE`. Make sure the gif actually renders and animates when viewing this README. (🚫 Remove this paragraph after after adding gif)
+
+<img src="YOUR_GIF_URL_HERE" width=250><br>
+
+### How to use
+
+1. First fill out the user name and password in timecard_beta.py file.
+2. Make sure you have a working python on you computer. Both Python 2 and Python 3 will work.
+3. Use pip to install selenium. You also need a webdriver so that the python can interact with your web browser. Right now, the only working one is Chrome. Go to the link below for more info. https://selenium-python.readthedocs.io/installation.html
+4. Open your terminal type python timecard_beta.py and type the day and the slot
+5. You should see a web window pop up and python will take care of the rest job.
+6. Enjoy!
+
--- timecard_beta.py
+++ timecard_beta.py
@@ -2,11 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 
+Weekday = input("Please enter a working day")
+hour_start = input("Please enter the start time you will work")
+hour_end = input("Please enter the end time of your work")
+
 driver = webdriver.Chrome()
 
 ##Get to the main page
-user = "bz15"
-pwd = "cexf28322832"
+user = "xxxx"
+pwd = "xxxx"
 
 driver.get("http://thehub.hampshire.edu")
 assert "TheHub Main Menu" in driver.title
@@ -35,37 +39,15 @@ submit_button.click()
 # Select the first timeslot
 # Need to have a better way to deal with the dates, now it's hard coding
 # Tuesday LIST_VAR_4_5, 4_19, Thursday 4_9, 4_23
-select = Select(driver.find_element_by_xpath("//select[@name='LIST.VAR4_5']"))
-select.select_by_visible_text('10:00AM')
-
-select = Select(driver.find_element_by_xpath('//select[@name="LIST.VAR5_5"]'))
-select.select_by_visible_text('02:00PM')
-
-select = Select(driver.find_element_by_xpath("//select[@name='LIST.VAR4_9']"))
-select.select_by_visible_text('10:00AM')
-
-select = Select(driver.find_element_by_xpath('//select[@name="LIST.VAR5_9"]'))
-select.select_by_visible_text('02:00PM')
-
-select = Select(driver.find_element_by_xpath("//select[@name='LIST.VAR4_19']"))
-select.select_by_visible_text('10:00AM')
-
-select = Select(driver.find_element_by_xpath('//select[@name="LIST.VAR5_19"]'))
-select.select_by_visible_text('02:00PM')
-
-select = Select(driver.find_element_by_xpath("//select[@name='LIST.VAR4_23']"))
-select.select_by_visible_text('10:00AM')
-
-select = Select(driver.find_element_by_xpath('//select[@name="LIST.VAR5_23"]'))
-select.select_by_visible_text('02:00PM')
-
-submit_button = driver.find_element_by_name('SUBMIT2')
-submit_button.click()
-
-select = Select(driver.find_element_by_name('VAR5'))
-select.select_by_value('L')
+# select = Select(driver.find_element_by_xpath("//select[@name='LIST.VAR4_5']"))
+# select.select_by_visible_text('10:00AM')
+el3 = driver.find_elements_by_xpath("//select")
 
 
+print(len(el3))
+date_list = {"Sunday": [0, 28], "Monday": [4, 32], "Tuesday": [8, 36], "Wednesday": [12, 40]}
+indice = date_list[Weekday]
 
-submit_button = driver.find_element_by_name('SUBMIT2')
-submit_button.click()
+for index in indice:
+	Select(el3[index]).select_by_visible_text(hour_start)
+	Select(el3[index+1]).select_by_visible_text(hour_end)
